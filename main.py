c25b84deb7ac7ef666728d8ce956b436e59e9e54
LettersDropped <letterdropped@tutanota.com>
2021-10-27 14:34:16 +0530

    Minor fixes

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import  random   #importing the random module to create random numbers
 
 
 ##Variables
-ques = input("Do you want to start this game?\nReply with 'y' for yes and 'n' for no\n")   #Asking questions to start the game
+ques = input("Do you want to start this game?\nReply with 'y' for yes and 'n' for no...\n")   #Asking questions to start the game
 yans = "y"              #Users answer is Yes
-repn = ":("             #Users answer is No
+nans = "n"              #Users answer is No
+repn = "See you soon!"             #Printing No screen
 guess = ''              #Empty variable to take the user's input
 
 randnum = (random.randint(0,9))     #generating random numbers upto 9 with random module
@@ -13,21 +14,19 @@ randnum = (random.randint(0,9))     #generating random numbers upto 9 with rando
 ##Main
 
 
-#Taking input from the User to start the game
+#Taking the user input and printing the results
 
 
-if ques == yans:
-    guess = input("Guess the number betwenn 0 to 9: ")
+if ques == yans:                                            #Running the if statements to print the results
+    guess = input("Guess the number betwenn 0 to 9: ")           
+    if guess == randnum:
+        print("You guessed the correct number!")
+    else:
+        print("Wrong Answer!, The correct answer is", randnum)
 else:
-    print(repn)                    
+    print(repn)         
 
 
-#Printing the results
 
-
-if guess == randnum:
-    print("You guessed the correct number!")
-else:
-    print("Wrong Answer!, The correct answer is", randnum)
 
 
