3912007ad46a18d1c031def168eb66527d13b234
LettersDropped <letterdropped@tutanota.com>
2021-12-19 17:54:21 +0530

    Cleaning

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import  random   #importing the random module to create random numbers   
+import  random     
 
+ques = input("Do you want to start this game?\nReply with 'y' for yes and 'n' for no...\n")   
+yans = "y"              
+nans = "n"              
+repn = "See you soon!"             
+guess = ''              
 
-##Variables
-ques = input("Do you want to start this game?\nReply with 'y' for yes and 'n' for no...\n")   #Asking questions to start the game
-yans = "y"              #Users answer is Yes
-nans = "n"              #Users answer is No
-repn = "See you soon!"             #Printing No screen
-guess = ''              #Empty variable to take the user's input
-
-randnum = (random.randint(0,9))     #generating random numbers upto 9 with random module
-
-
-#Taking the user input and printing the results running if-else statements to print the results..
+randnum = (random.randint(0,9))     
 
 if ques == yans:                         
     print("Okay, I am guessing the number between 0 to 9... Enter your number:")                                            #Running the if statements to print the results
